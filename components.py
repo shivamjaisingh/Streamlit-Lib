9c69ea1a6c43d7f629d11eab8a0bcef6ab8d78b7
Shivam Jai Singh <20377780+shivamjaisingh@users.noreply.github.com>
2024-05-22 02:22:48 +0000

    added structures

--- components.py
+++ components.py
@@ -203,3 +203,17 @@ with st.form("Order Form"):
 
 st.write("These values, {}, {}, and {}, that were set inside the form are\
          accessible outside the form".format(quantity, fruit, city))
+
+
+sample_data = [3.12, -4.31, 6.76, -9.88, 1.09]
+col1, col2, col3 = st.columns([2.6,5,3.8])
+
+col1.subheader("Line Chart")
+col1.line_chart(sample_data)
+
+col2.subheader("Area Chart")
+col2.area_chart(sample_data)
+
+with col3:
+    st.subheader("Bar Chart")
+    st.bar_chart(sample_data)
