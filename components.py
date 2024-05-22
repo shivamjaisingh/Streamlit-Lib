6b03b6f895b9c7f2d7850209e439e93c5093c35f
Shivam Jai Singh <20377780+shivamjaisingh@users.noreply.github.com>
2024-05-22 02:19:52 +0000

    added forms

--- components.py
+++ components.py
@@ -186,3 +186,20 @@ st.download_button("Download data",
                    df.to_csv(index=False),
                    file_name="data.csv")
 
+with st.form("Order Form"):
+    st.write("Order Details")
+    fruit = st.select_slider("Select a fruit",
+                             ["Banana", "Pawpaw", "Guava", "Mango"])
+
+    quantity = st.number_input("Select the number of fruits", min_value=0)
+
+    city = st.text_input("Type in your city")
+
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        st.write("You have ordered {} {}(s) to be picked up at the {} branch"
+                 .format(quantity, fruit, city))
+
+st.write("These values, {}, {}, and {}, that were set inside the form are\
+         accessible outside the form".format(quantity, fruit, city))
