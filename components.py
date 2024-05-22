d19285f64533b979e3229e914e2dd66e31ccf737
Shivam Singh <shivamsingh94192@gmaill.com>
2024-05-21 21:10:00 -0400

    removed matplotlib

--- components.py
+++ components.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
 import time
@@ -36,17 +35,7 @@ st.bar_chart(df)
 arr1, arr2 = np.random.randn(100), np.random.rand(100)
 arr3 = np.random.randint(1,1001,100)
 
-fig, ax = plt.subplots()
-ax.set_title("Matplotlib Scatter Plot")
-ax.scatter(arr1, arr2, s = arr3, c = arr3, alpha = 0.6)
-st.pyplot(fig)
 
-
-
-fig, ax = plt.subplots()
-ax.set_title("Seaborn Scatter Plot")
-ax = sns.scatterplot(arr1)
-st.pyplot(fig)
 
 st.image("https://bit.ly/edus3ha")
 st.video("https://bit.ly/canibs3")
